76cbf2b04b3f0877c36f2847e8c044aeca29c7bd
Zihong Hao <haozihong@hotmail.com>
2024-04-08 19:12:05 -0400

    some plugins from previous setting

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,26 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+  -- { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+
+  { import = "astrocommunity.completion.codeium-vim" },
+
+  { import = "astrocommunity.motion.nvim-surround" },
+
+  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },  -- require neovim v0.10
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  {
+    "karb94/neoscroll.nvim",
+    opts = {
+      easing_function = "circular",
+    },
+  },
+  -- { import = "astrocommunity.scrolling.satellite-nvim" },  -- require neovim v0.10
+  { import = "astrocommunity.utility.noice-nvim" },
 }
