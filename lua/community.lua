6739e0d52524dec2fa78529aea03805bd0d1f853
Zihong Hao <haozihong@hotmail.com>
2025-10-18 16:37:17 -0700

    feat: add or enable some plugins
    Add or enable plugins: dropbar-nvim, satellite-nvim, noice-nvim,
    flash-nvim, astrocommunity.recipes.vscode

--- lua/community.lua
+++ lua/community.lua
@@ -21,7 +21,7 @@ return {
 
   { import = "astrocommunity.motion.nvim-surround" },
 
-  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },  -- require neovim v0.10
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
   { import = "astrocommunity.scrolling.neoscroll-nvim" },
   {
     "karb94/neoscroll.nvim",
@@ -29,6 +29,8 @@ return {
       easing_function = "circular",
     },
   },
-  -- { import = "astrocommunity.scrolling.satellite-nvim" },  -- require neovim v0.10
-  -- { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.scrolling.satellite-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.recipes.vscode" },
 }
